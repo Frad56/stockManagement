17def6e089181b59b3c49f36b2ed871f02f43445
Frad56 <fradd.sondes@sousse.r-iset.tn>
2026-01-22 22:57:42 +0100

    Create Repository test

--- src/main/java/com/example/store/Repository/PlaceRepository.java
+++ src/main/java/com/example/store/Repository/PlaceRepository.java
@@ -4,7 +4,11 @@ import com.example.store.Model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
+
+    Optional<Place> findPlaceByName(String name);
 }
